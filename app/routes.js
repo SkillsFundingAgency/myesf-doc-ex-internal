6169b8c8c4a97e8bf029f604901e0b19a4cac9f7
Keith Symonds <keith.symonds@methods.co.uk>
2021-01-05 12:20:33 +0000

    Updates to mitigate WAF issue
    Updates to mitigate WAF issue

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+// Add your routes below here - above the module.exports line
+
+// Branching for publish 14 to 16 document type
+router.post('/v7/document-exchange/publish-14-to-16-answer', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+  
+    const publish14to16 = req.session.data['publish-14-to-16']
+  
+    if (publish14to16 === 'true') {
+      res.redirect('/v7/document-exchange/confirmation-14to16')
+    } else {
+      res.redirect('/v7/document-exchange/publish-documents-new')
+    }
+  })
+
+  // Add your routes here - above the module.exports line
 
 module.exports = router
